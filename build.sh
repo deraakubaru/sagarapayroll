6f80cc9f19a359ad34a64821683a3b3ccdba3d88
Dera Akbar <deraakbar69@gmail.com>
2023-09-01 09:05:26 +0700

    still trying(2)

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.11.4 -m pip install -r requirements.txt
+py -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3.11.4 manage.py makemigrations --noinput
-python3.11.4 manage.py migrate --noinput
+py manage.py makemigrations --noinput
+py manage.py migrate --noinput
 
 echo "Collect Static..."
-Python3.11.4 manage.py collectstatic --noinput --clear
+py manage.py collectstatic --noinput --clear
